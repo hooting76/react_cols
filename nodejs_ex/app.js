fb1152a5bd2d6db688b7282ebfca5968641d4132
jch <ggongco1120@gmail.com>
2025-05-28 17:45:48 +0900

    0528/nodejs/express start / routes

--- nodejs_ex/app.js
+++ nodejs_ex/app.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// routes selectors call(require)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var backendRouter = require('./routes/backend');
+
+// add a CORS 
+const cors = require('cors');
 
 var app = express();
 
@@ -19,8 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes path name defined.
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/backend', backendRouter);
+
+// add a CORS 
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
